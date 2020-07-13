1f59a6140d258d5f6af3fedb4dea9b77a6c31100
cbrock <colin.brock@lytics.com>
2020-07-13 14:48:05 -0700

    Implement solution for go/bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,15 +1,31 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package bob provides responses to conversational input
 package bob
 
-// Hey should have a comment documenting it.
+import (
+	"regexp"
+	"strings"
+)
+
+// Hey accepts a remark and prints a lackadaisical response
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	trimmedRemark := strings.TrimSpace(remark)
+
+	if trimmedRemark == "" {
+		return "Fine. Be that way!"
+	}
+
+	hasAlpha, _ := regexp.MatchString(`[A-Za-z]`, trimmedRemark)
+	isAllUpperCase := hasAlpha && strings.ToUpper(trimmedRemark) == trimmedRemark
+	isQuestion := strings.HasSuffix(trimmedRemark, "?")
+	isExclamation := strings.HasSuffix(trimmedRemark, "!")
+
+	if !isAllUpperCase && isQuestion {
+		return "Sure."
+	} else if (isAllUpperCase && isExclamation) || (isAllUpperCase && !isExclamation && !isQuestion) {
+		return "Whoa, chill out!"
+	} else if isAllUpperCase && isQuestion {
+		return "Calm down, I know what I'm doing!"
+	} else {
+		return "Whatever."
+	}
 }
